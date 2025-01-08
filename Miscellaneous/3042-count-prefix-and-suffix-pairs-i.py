959454f1c23dcf9265a3770a9460fdc381993f70
Muhammad Maaz <mmaaz2005@hotmail.com>
2025-01-08 23:58:19 +0500

    Added solutions for 1 LeetCode problem: 1 (q3042) from Daily Streak.

---
+++ Miscellaneous/3042-count-prefix-and-suffix-pairs-i.py
@@ -0,0 +1,16 @@
+class Solution:
+    def countPrefixSuffixPairs(self, words: List[str]) -> int:
+        count = 0
+        n = len(words)
+        
+        # Iterate over all possible pairs (i, j) where i < j
+        for i in range(n):
+            for j in range(i + 1, n):
+                prefix = words[i]
+                target = words[j]
+                
+                # Check if 'prefix' is both a prefix and suffix of 'target'
+                if target.startswith(prefix) and target.endswith(prefix):
+                    count += 1
+                    
+        return count
