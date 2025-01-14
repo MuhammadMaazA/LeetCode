4c129ce87b25cb2b25dc49b8c0aa17bb0af31ca9
Muhammad Maaz <mmaaz2005@hotmail.com>
2025-01-14 21:24:02 +0500

    Revised solutions for 1 LeetCode problem: 1 (q3223) from Daily Streak.

--- Miscellaneous/3223-minimum-length-of-string-after-operations.py
+++ Miscellaneous/3223-minimum-length-of-string-after-operations.py
@@ -1,18 +1,10 @@
 class Solution:
     def minimumLength(self, s: str) -> int:
-        from collections import Counter
+        res = 0
         
-        freq = Counter(s)
-        ans = 0
-        
-        for char, count in freq.items():
-            if count == 0:
-                continue
-            if count % 2 == 1:
-                # odd frequency -> 1 remains
-                ans += 1
+        for cnt in Counter(s).values():
+            if cnt % 2 == 1:
+                res += 1
             else:
-                # even frequency -> 2 remain
-                ans += 2
-        
-        return ans
+                res += 2
+        return res
