4e5b5c923f3bf1c8fe53bb7d5fc64dbc677c4cf2
Muhammad Maaz <mmaaz2005@hotmail.com>
2025-01-16 09:10:22 +0000

    Revised solution for 1 LeetCode problem: 1 (q2429) from Daily Streak.

--- Miscellaneous/2429-minimize-xor.py
+++ Miscellaneous/2429-minimize-xor.py
@@ -1,13 +1,21 @@
 class Solution:
     def minimizeXor(self, num1: int, num2: int) -> int:
-        cnt = num2.bit_count()
-        x = 0
-        for i in range(30, -1, -1):
-            if num1 >> i & 1 and cnt:
-                x |= 1 << i
-                cnt -= 1
-        for i in range(30):
-            if num1 >> i & 1 ^ 1 and cnt:
-                x |= 1 << i
-                cnt -= 1
+        def count_bits(n):
+            res = 0
+            while n > 0:
+                res += 1 & n 
+                n = n >> 1 
+            return res
+        cnt1, cnt2 = count_bits(num1), count_bits(num2)
+        x = num1 
+        i = 0
+        while cnt1 != cnt2:
+            if cnt1 > cnt2 and x & (1 << i):
+                cnt1 -= 1
+                x = x ^ (1 << i)
+            if cnt1 < cnt2 and x & (1 << i) == 0:
+                cnt1 += 1
+                x = x | (1 << i)
+            i += 1
         return x
+
