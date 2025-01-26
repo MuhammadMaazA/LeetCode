682e7f55007475b8570b97174caae8f6640aa240
Muhammad Maaz <mmaaz2005@hotmail.com>
2025-01-26 21:55:29 +0000

    Added solution for LeetCode problem 48 (Rotate Image)

---
+++ Arrays_and_Strings/48-rotate-image.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+
+        # Transpose the matrix
+        for i in range(n):
+            for j in range(i+1,n): # i+1 to avoid swapping the same element twice
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        # Reverse the rows
+        for i in range(n):
+            for j in range(n//2):
+                matrix[i][j], matrix[i][n-j-1] = matrix[i][n-j-1], matrix[i][j]
