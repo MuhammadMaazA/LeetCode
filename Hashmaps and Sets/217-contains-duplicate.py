f71d8290835da8f121244ae72973bfcdc3a20a4d
Muhammad Maaz <mmaaz2005@hotmail.com>
2025-01-27 12:14:50 +0000

    Added time and space complexity comments to solution for LeetCode problem 217 (Contains Duplicate)

--- Hashmaps and Sets/217-contains-duplicate.py
+++ Hashmaps and Sets/217-contains-duplicate.py
@@ -6,3 +6,6 @@ class Solution:
             return True
         else:
             return False
+        
+        # Time: O(n) because set() operation is O(n) where n is the number of elements in nums
+        # Space: O(n) because we are storing all the elements of nums in a set n elements
