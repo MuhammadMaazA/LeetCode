87aadb2cb1550b9252f8e1a8e9d62aceb59a260c
Muhammad Maaz <mmaaz2005@hotmail.com>
2025-02-04 11:18:32 +0000

    Added solution for LeetCode problem 169 (Majority Element)

---
+++ Hashmaps and Sets/169-majority-element.py
@@ -0,0 +1,15 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        # Using Boyer Moore Voting Algorithm 
+        candidate, count = None, 0
+        for num in nums:
+            if count == 0:
+                candidate, count = num, 1
+            elif num == candidate:
+                count += 1
+            else:
+                count -= 1
+        return candidate
+    
+    # Time: O(n) because we traverse teh array only once
+    # Space: O(1) because we use only two variables candidate and count
