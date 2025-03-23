4b5fa0d74023f6b36ffde94567dd42d2243feafe
Muhammad Maaz <mmaaz2005@hotmail.com>
2025-03-23 13:37:31 +0000

    Add solution for LeetCode problem 876 (Middle of the Linked List)

---
+++ Linked Lists/876-middle-of-the-linked-list.py
@@ -0,0 +1,15 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = fast = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+        return slow
+    
+    # Time: O(n) each pointer goes through n nodes in worst case
+    # Space: O(1) we only use two pointers regardless of list size
